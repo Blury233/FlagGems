12c3909805bbd645e6aec9db251df05ca145eaae
Blury233 <blurylee@outlook.com>
2025-09-11 07:50:11 +0000

    fix issue

--- tests/test_unary_pointwise_ops.py
+++ tests/test_unary_pointwise_ops.py
@@ -359,6 +359,57 @@ def test_accuracy_elu(shape, dtype):
     gems_assert_close(res_out, ref_out, dtype)
 
 
+@pytest.mark.inplace
+@pytest.mark.elu_
+@pytest.mark.parametrize("shape", POINTWISE_SHAPES)
+@pytest.mark.parametrize("dtype", FLOAT_DTYPES)
+def test_accuracy_elu_(shape, dtype):
+    inp = torch.randn(shape, dtype=dtype, device=flag_gems.device)
+    alpha = torch.rand(1).item()
+
+    res_inp = inp.clone().to(flag_gems.device)
+    inp_clone = inp.clone()
+    ref_inp = to_reference(inp_clone, True)
+    torch.nn.functional.elu_(ref_inp, alpha)
+
+    with flag_gems.use_gems():
+        torch.nn.functional.elu_(res_inp, alpha)
+
+    gems_assert_close(res_inp, ref_inp, dtype)
+
+
+@pytest.mark.elu_backward
+@pytest.mark.parametrize("shape", POINTWISE_SHAPES)
+@pytest.mark.parametrize("dtype", FLOAT_DTYPES)
+@pytest.mark.parametrize("is_result", [True, False])
+def test_accuracy_elu_backward(shape, dtype, is_result):
+    alpha = torch.rand(1).item()
+    scale = 1.0
+    input_scale = 1.0
+
+    res_inp = torch.randn(shape, dtype=dtype, device=flag_gems.device)
+    res_grad_out = torch.randn_like(res_inp)
+
+    if is_result:
+        res_self_or_result = torch.ops.aten.elu(res_inp, alpha, scale, input_scale)
+    else:
+        res_self_or_result = res_inp
+
+    ref_grad_out = to_reference(res_grad_out, True)
+    ref_self_or_result = to_reference(res_self_or_result, True)
+
+    ref_in_grad = torch.ops.aten.elu_backward(
+        ref_grad_out, alpha, scale, input_scale, is_result, ref_self_or_result
+    )
+
+    with flag_gems.use_gems():
+        res_in_grad = torch.ops.aten.elu_backward(
+            res_grad_out, alpha, scale, input_scale, is_result, res_self_or_result
+        )
+
+    gems_assert_close(res_in_grad, ref_in_grad, dtype)
+
+
 @pytest.mark.relu
 @pytest.mark.parametrize("shape", POINTWISE_SHAPES)
 @pytest.mark.parametrize("dtype", FLOAT_DTYPES)
